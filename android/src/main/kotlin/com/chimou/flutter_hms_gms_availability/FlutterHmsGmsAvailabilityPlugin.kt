d56569e35f5a1d3f48f33269401f56e7b94e7f4b
rgoksu19 <149580220+rgoksu19@users.noreply.github.com>
2025-10-01 13:57:25 +0300

    Update FlutterHmsGmsAvailabilityPlugin.kt
    registrat issue modified

--- android/src/main/kotlin/com/chimou/flutter_hms_gms_availability/FlutterHmsGmsAvailabilityPlugin.kt
+++ android/src/main/kotlin/com/chimou/flutter_hms_gms_availability/FlutterHmsGmsAvailabilityPlugin.kt
@@ -6,7 +6,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+//import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.content.Context
 import com.huawei.hms.api.ConnectionResult
 import com.huawei.hms.api.HuaweiApiAvailability
